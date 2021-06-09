c0cbff5db5917df05f9ac7a2a6268b2900687c2a
monaelufabc <monael@ufabc.edu.br>
2021-06-09 21:39:59 +0000

    Fontes aula 08/06/2021

---
+++ na3/Fatorial/Fatorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fatorial
+{
+    public static void main(String[] args)
+    {
+        int n;
+        long fat = 1l;
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+
+        while(n > 0)
+        {
+            fat = fat * n; // fat *= n;
+            n = n - 1;
+        }
+        System.out.println(fat);
+    }
+}
---
+++ na3/NotasEMedia/NotasEMedia.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NotasEMedia
+{
+    public static void main(String[] args)
+    {
+        float nota, somaNotas = 0f;
+        int i;
+        Scanner scan = new Scanner(System.in);
+
+        i = 0;
+        while(i < 10)
+        {
+            System.out.println("Informe a " + (i+1) + "a. nota:");
+            nota = scan.nextFloat();
+            somaNotas = somaNotas + nota;
+            i = i + 1;
+        }
+        System.out.printf("Sua Media Final dos Trabalhos (NFT) foi %.2f\n", (somaNotas / 10f));
+    }
+}
