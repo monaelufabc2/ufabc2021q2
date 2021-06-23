f4c8bcfa4ac33dd31b64963cbfbb4af8462bb1e8
monaelufabc <monael@ufabc.edu.br>
2021-06-23 02:19:40 +0000

    fontes das aulas de 22/06/2021

---
+++ nb2/Exemplos/Blocos/Blocos.java
@@ -0,0 +1,43 @@
+public class Blocos
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i < 3)
+            i++;
+            System.out.println("Este comando repetira 3x");
+            System.out.println("Este tambem!");
+    }
+}
+
+/*
+public class Blocos
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i < 3)
+        {
+            i++;
+        }
+        System.out.println("Este comando repetira 3x");
+        System.out.println("Este tambem!");
+    }
+}
+*/
+
+/*
+public class Blocos
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i < 3);
+        {
+            i++;
+            System.out.println("Este comando repetira 3x");
+            System.out.println("Este tambem!");
+        }
+    }
+}
+*/
---
+++ nb2/Exemplos/Funcao/Funcao.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Funcao
+{
+    public static void main(String []args)
+    {
+        int x, y;
+        Scanner scan = new Scanner(System.in);
+        x = scan.nextInt();
+        y = scan.nextInt();
+        System.out.println("x = " + x + " y = " + y + " misterio() = " + misterio(x, y));
+    }
+
+    public static int misterio(int x, int y)
+    {
+        while(x>0)
+        {
+            y=magica(y);
+            x=ilusao(x);
+        }
+        return y;
+    }
+
+    public static int magica(int x)
+    {
+        return x+1;
+    }
+
+    public static int ilusao(int x)
+    {
+        return x-1;
+    }
+}
