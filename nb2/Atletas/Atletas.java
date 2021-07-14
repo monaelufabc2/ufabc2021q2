1ba8518824f35d0f370ddb6f71a34432fbf689a0
monaelufabc <monael@ufabc.edu.br>
2021-07-14 22:20:05 +0000

    Fontes aula 13/07/2021

---
+++ nb2/Atletas/Atletas.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Atletas
+{
+    public static final int TAMANHO = 100;
+
+    public static void main(String[] args)
+    {
+        int idades[] = new int[TAMANHO], i, qtdMasc = 0, qtdFem = 0, qtdMascAcimaMedAltura = 0, qtdFemAbaixoMedPeso = 0;
+        float alturas[] = new float[TAMANHO], somaAlturaMasc = 0f, mediaAltMasc;
+        float pesos[] = new float[TAMANHO], somaPesoFem = 0f, mediaPesoFem;
+        char generos[] = new char[TAMANHO];
+        Scanner scan = new Scanner(System.in);
+
+        for(i=0; i<TAMANHO; i++)
+        {
+            //System.out.print("Informe a idade do " + (i+1) + "o. atleta: ");
+            idades[i] = scan.nextInt();
+            //System.out.print("Informe a altura do " + (i+1) + "o. atleta: ");
+            alturas[i] = scan.nextFloat();
+            //System.out.print("Informe o peso do " + (i+1) + "o. atleta: ");
+            pesos[i] = scan.nextFloat();
+            //System.out.print("Informe o genero do " + (i+1) + "o. atleta: ");
+            generos[i] = scan.next().charAt(0);
+
+            if(generos[i] == 'M')
+            {
+                somaAlturaMasc = somaAlturaMasc + alturas[i];
+                qtdMasc = qtdMasc + 1;
+            }
+            else
+            {
+                somaPesoFem = somaPesoFem + pesos[i];
+                qtdFem = qtdFem + 1;
+            }
+        }
+
+        System.out.println(somaAlturaMasc);
+        System.out.println(qtdMasc);
+        System.out.println(somaPesoFem);
+        System.out.println(qtdFem);
+
+        mediaAltMasc = somaAlturaMasc/qtdMasc;
+        mediaPesoFem = somaPesoFem/qtdFem;
+
+        for(i=0; i<TAMANHO; i++)
+        {
+            if(generos[i] == 'M' && alturas[i] > mediaAltMasc)
+            {
+                qtdMascAcimaMedAltura = qtdMascAcimaMedAltura + 1;
+            }
+
+            if(generos[i] == 'F' && pesos[i] < mediaPesoFem)
+            {
+                qtdFemAbaixoMedPeso = qtdFemAbaixoMedPeso + 1;
+            }
+        }
+
+        if(qtdMasc != 0)
+        {
+            System.out.println("Ha " + qtdMascAcimaMedAltura + " atletas homens acima da media da altura dos homens.");
+        }
+        else
+        {
+            System.out.println("Nao houve entrada de atletas homens.");
+        }
+
+        if(qtdFem != 0)
+        {
+            System.out.println("Ha " + qtdFemAbaixoMedPeso + " atletas mulheres abaixo da media do peso das mulheres.");
+        }
+        else
+        {
+            System.out.println("Nao houve entrada de atletas mulheres.");
+        }
+    }
+}
---
+++ nb2/Atletas/GeraEntrada.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class GeraEntrada
+{
+    public static void main(String[] args)
+    {
+        int i;
+        Random rd = new Random();
+
+        for(i=0; i<100; i++)
+        {
+            System.out.println(rd.nextInt(91)+10);
+            System.out.println(rd.nextInt(3) + "." + rd.nextInt(100));
+            System.out.println(rd.nextInt(100) + "." + rd.nextInt(100));
+            if(i<99)
+            {
+                if(args[0].compareTo("A") == 0)
+                {
+                    System.out.println(rd.nextBoolean()?"M":"F");
+                }
+                else
+                {
+                    System.out.println(args[0]);
+                }
+            }
+            else
+            {
+                if(args[0].compareTo("A") == 0)
+                {
+                    System.out.print(rd.nextBoolean()?"M":"F");
+                }
+                else
+                {
+                    System.out.print(args[0]);
+                }
+            }
+        }
+    }
+}
